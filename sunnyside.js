b6237f1f90f758352bb6b5568d6e6de6b1650275
omarProgrammer21 <o214729@gmail.com>
2023-04-03 18:02:52 +0300

    Initial commit

---
+++ sunnyside.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    
+});
